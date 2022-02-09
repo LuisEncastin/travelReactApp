48543279a2bb96890527b941212f60c28fc9f2ed
LuisEncastin <luisencastin@gmail>
2022-02-08 22:07:19 -0600

    Primer avance de componentes dummy

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import Header from './components/Header/Header.jsx';
+import List from './components/List/List.jsx';
+import Map from './components/Map/Map.jsx';
+import PlaceDetails from './components/PlaceDetails/PlaceDetails.jsx';
+
+const App = () => {
+    return (
+        <div>
+            <h1>Hello World!</h1>
+            <Header/>
+            <List/>
+            <Map/>
+            <PlaceDetails/>
+        </div>
+    );
+};
 
 export default App;
